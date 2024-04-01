77facd82938028af106d240ebc8ffd2bd1d577e6
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-04-01 10:39:34 +0600

    Get A Logged-In User Info From The Session

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -6,19 +6,23 @@ const DashboardPage = async () => {
   const session = await getServerSession(authOptions);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">
-        Welcome To {session?.user?.name}
-      </h1>
-      <h1 className="text-4xl text-center mt-10">
-        Logged-in user email:{session?.user?.email}
-      </h1>
-      <Image
-        src={session?.user?.image}
-        width={100}
-        height={100}
-        alt="user image"
-        className="mx-auto"
-      />
+      {session?.user && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome To {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Logged-in user email:{session?.user?.email}
+          </h1>
+          <Image
+            src={session?.user?.image}
+            width={100}
+            height={100}
+            alt="user image"
+            className="mx-auto rounded-full mt-5"
+          />
+        </>
+      )}
     </div>
   );
 };
